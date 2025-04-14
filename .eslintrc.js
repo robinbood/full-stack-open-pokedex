6c4471b05e09fe576b9c2814d52ec13ba627172e
robinbood <mercedesfarts@gmail.com>
2025-04-14 12:55:56 +0500

    lint the files

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,14 +23,8 @@ module.exports = {
       "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
+    
+    
     "semi": [
       "error",
       "never"
